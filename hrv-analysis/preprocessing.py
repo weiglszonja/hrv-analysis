f44c1573fb92c05fe343e215bfc41872e0c8f913
Szonja Weigl <wszonja@gmail.com>
2019-05-07 20:51:32 +0200

    Add peak detection to preprocessing module

--- hrv-analysis/preprocessing.py
+++ hrv-analysis/preprocessing.py
@@ -1,9 +1,11 @@
 import os
+
 import mne
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from scipy.signal import butter, filtfilt, freqz
+from scipy.signal import find_peaks
 
 import logging
 logging.getLogger().setLevel(logging.INFO)
@@ -107,3 +109,90 @@ class SignalPreprocessing:
 
         logging.info('Reprocessing finished')
         return integrated_data
+
+
+class PeakDetection:
+    '''
+    Detects peaks on preprocessed data based on Pan Tompkins algorithm and returns peak intervals in milliseconds.
+
+    Reference:
+    J. Pan and W. J. Tompkins, "A Real-Time QRS Detection Algorithm,"  in IEEE Transactions on Biomedical Engineering,
+     vol. BME-32, no. 3, pp. 230-236, March 1985. doi: 10.1109/TBME.1985.325532
+    '''
+
+    def __init__(self, data, fs):
+        self.fs = fs
+        self.rr = self._detect_peaks(data)
+
+    @staticmethod
+    def _read_config():
+        return pd.read_json(CONFIG)['peak_detection'].to_dict()
+
+    def _detect_peaks(self, data):
+        config = self._read_config()
+        qrs_period = config['qrs_period'] * self.fs
+        refractory_period = config['refractory_period'] * self.fs
+
+        data = data[self.fs: (self.fs * 5 * 60)]
+        rr = []
+
+        def _batch(iterable, batch_length=1):
+            iterable = list(iterable)
+            ln = len(iterable)
+            for ndx in range(0, ln, batch_length):
+                yield np.asarray([e for e in iterable[ndx:min(ndx + batch_length, ln)]])
+
+        def _get_rr_intervals_in_ms(rr_peak_indices, rr_min, rr_max):
+            rr_intervals = np.diff(rr_peak_indices)
+            rr_intervals = [(rri * 1000) / self.fs for rri in rr_intervals]
+            rr_intervals = np.asarray(rr_intervals)
+            return rr_intervals[(rr_intervals > rr_min) * (rr_intervals < rr_max)]
+
+        epoch_length = int(self.fs * 30)
+        epoch_num = 0
+        for epoch in _batch(data, epoch_length):
+            epoch_num += 1
+            epoch_peaks = []
+
+            last_peak = 0
+            threshold_value = 0.0
+            signal_peak_value = 0.0
+            noise_peak_value = 0.0
+
+            normalized_epoch = (epoch - min(epoch)) / (max(epoch) - min(epoch))
+            peak_candidates, _ = find_peaks(normalized_epoch, height=0.1, distance=self.fs * 0.4)
+            if len(peak_candidates):
+                for ind, peak in enumerate(peak_candidates):
+                    if qrs_period < (peak - last_peak) < refractory_period or not len(epoch_peaks):
+                        if epoch[peak] > threshold_value:
+                            epoch_peaks.append(peak)
+                            signal_peak_value = 0.125 * epoch[peak] + 0.875 * signal_peak_value
+                        else:
+                            noise_peak_value = 0.125 * epoch[peak] + 0.875 * noise_peak_value
+
+                        threshold_value = noise_peak_value + 0.25 * (signal_peak_value - noise_peak_value)
+
+                    else:
+                        signal_peak_value = 0.5 * signal_peak_value
+                        threshold_value = noise_peak_value + 0.25 * (signal_peak_value - noise_peak_value)
+                        if epoch[peak] > threshold_value:
+                            epoch_peaks.append(peak)
+
+                    last_peak = epoch_peaks[-1]
+
+                epoch_rr_intervals = _get_rr_intervals_in_ms(rr_peak_indices=epoch_peaks, rr_min=400.0, rr_max=1400.0)
+
+                rr.extend(epoch_rr_intervals)
+
+            else:
+                logging.info(f'No peak candidates were found in epoch {epoch_num}!')
+
+            if eval(config['plot']):
+                plt.figure()
+                plt.plot(normalized_epoch)
+                plt.plot(epoch_peaks, normalized_epoch[epoch_peaks], '*')
+                plt.show()
+
+        logging.info(f'Average RR interval in data: {np.mean(rr)} ms')
+
+        return rr

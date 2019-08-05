4915055af6081531273ec7f1d466df8926588bbd
weiglszonja <szonja@synetiq.net>
2019-08-05 11:59:13 +0200

    Add sample script and data for executing analysis

---
+++ run_analysis.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+from datetime import datetime
+from utils.preprocessing import SignalPreprocessing, PeakDetection
+from utils.feature_extraction import TimeDomainMetrics, FrequencyDomainMetrics
+
+source_path = os.getenv('SOURCE')
+target_path = os.getenv('TARGET')
+
+subjects = os.listdir(source_path)
+headers = ['asrt_1_1', 'asrt_1_2', 'asrt_1_3', 'asrt_1_4',
+           'asrt_1_5', 'asrt_2', 'asrt_3_1', 'asrt_3_2']
+
+
+def main():
+    subjects_data = pd.DataFrame()
+    for subject in subjects:
+        for header in headers:
+            subject_data = pd.DataFrame({'subject': [subject], 'header': [header], 'id': [f'{subject}_{header}']})
+
+            preprocessing = SignalPreprocessing(
+                data_path=os.path.join(source_path, subject, f'{subject}_{header}.vhdr'))
+
+            peak_detection = PeakDetection(data=preprocessing.preprocessed_data,
+                                           fs=preprocessing.fs,
+                                           filtered_data=preprocessing.filtered_data)
+
+            time_domain_metrics = TimeDomainMetrics(rr=peak_detection.rr,
+                                                    fs=preprocessing.fs)
+
+            frequency_domain_metrics = FrequencyDomainMetrics(rr=peak_detection.rr,
+                                                              fs=preprocessing.fs)
+
+            subject_data = subject_data.assign(**{**time_domain_metrics.metrics, **frequency_domain_metrics.metrics})
+            subjects_data = subjects_data.append(subject_data, ignore_index=True)
+
+    dt = datetime.utcnow().strftime("%Y%m%d")
+    target = os.path.join(target_path, dt, 'hrv')
+    os.makedirs(target, exist_ok=True)
+    subjects_data.to_csv(os.path.join(target, f'{dt}_subjects_hrv.csv'))
+
+
+if __name__ == '__main__':
+    main()
